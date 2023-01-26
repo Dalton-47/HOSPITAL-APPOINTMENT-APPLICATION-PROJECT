11aef6dde48cf2d5075476c14a79851cd929e39e
DALTON LEYIAN <daltonleyian@gmail.com>
2023-01-26 14:02:57 +0300

    Made changes to the recyclerView to save messages from the patient and update in firebase

--- app/src/main/java/com/example/my_hospital_appointments/Chattingpage.java
+++ app/src/main/java/com/example/my_hospital_appointments/Chattingpage.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,28 +16,43 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Chattingpage extends AppCompatActivity {
-    public ArrayList<String> data=new ArrayList<>();
+
+    public ArrayList<String> messages_from_user=new ArrayList<>();
+    ArrayList <Patient_Messages> myList=new ArrayList();
+    ArrayList<String> data=new ArrayList<>();
     Button myButton;
     DatabaseReference userMessages;
     String messageKey;
     String usersMessage;
     long timestamp;
     String senderID;
+    String doctorName;
+    chattingAdapter  myAdapter;
+    int counter=0;
+    String userEmailKey;
+    LinearLayoutManager layoutManager;
+    RecyclerView myRecycleViewer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chattingpage);
 
+
         //getting the data passed from previous activity
-        String userEmailKey=getIntent().getStringExtra("emailKey");
-        String doctorName=getIntent().getStringExtra("doctorsName");
+         userEmailKey=getIntent().getStringExtra("emailKey");
+         doctorName=getIntent().getStringExtra("doctorsName");
 
         Toast.makeText(Chattingpage.this,"user's key = "+userEmailKey+" Doc name = "+doctorName,Toast.LENGTH_SHORT).show();
         FirebaseAuth myAuth=FirebaseAuth.getInstance();
@@ -45,31 +61,55 @@ public class Chattingpage extends AppCompatActivity {
 
         DatabaseReference myDoctors= FirebaseDatabase.getInstance().getReference(userId);
          userMessages=FirebaseDatabase.getInstance().getReference("PATIENTMESSAGES").child(userEmailKey).child(doctorName);
+        //now we generate unique ID's to store messages from the sender
 
-         messageKey=FirebaseDatabase.getInstance().getReference("PATIENTMESSAGES").child(userEmailKey).child(doctorName).push().getKey();
 
-        RecyclerView myRecycleViewer = findViewById(R.id.recyclerViewChat);
-        chattingAdapter  myAdapter=new chattingAdapter(data);
+        //
+
+         myRecycleViewer = findViewById(R.id.recyclerViewChat);
+
+        readPatientMessages();
+        myAdapter=new chattingAdapter(messages_from_user);
         myRecycleViewer.setAdapter(myAdapter);
+      //  myRecycleViewer.smoothScrollToPosition(myAdapter.getItemCount()-1); //to set the recyclerView items to the last position
 
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+
+         layoutManager=new LinearLayoutManager(this);
+        layoutManager.setStackFromEnd(true);//to set the recyclerView items to the last position
         myRecycleViewer.setLayoutManager(layoutManager);
+
+
+        EditText myEditText= (EditText) findViewById(R.id.editTextTypeMessageChat);
+        String nullText=myEditText.getText().toString().trim(); //get text that is available before user clicks anything
+
+
 
         myButton=(Button) findViewById(R.id.buttonSendMessageChat);
 
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText myEditText= (EditText) findViewById(R.id.editTextTypeMessageChat);
-                 usersMessage=myEditText.getText().toString().trim();//get users message only after they press send
-                data.add(usersMessage);
-                myAdapter.notifyDataSetChanged();
+                     usersMessage=myEditText.getText().toString().trim();//get users message only after they press send
+               // data.add(usersMessage);
+
+                //messages_from_user.add(usersMessage);
+               // data.add(usersMessage);
+                //myAdapter.setData(data);
 
                timestamp=System.currentTimeMillis();//get the timestamp for when the user was sending the message
                senderID ="patient";
 
                 myEditText.setText("");
-                addMessage();
+                if(!Objects.equals(usersMessage, nullText))
+                {
+                    addMessage();
+                }
+                else
+                {
+                    myEditText.setError("MESSAGE CANNOT BE BLANK");
+                }
+
+               // readPatientMessages();
             }
         });
 
@@ -77,6 +117,9 @@ public class Chattingpage extends AppCompatActivity {
 
      private void addMessage()
     {
+        messageKey=FirebaseDatabase.getInstance().getReference("PATIENTMESSAGES").child(userEmailKey).child(doctorName).push().getKey();
+
+
         String myTime=Long.toString(timestamp);
         Patient_Messages userMessagesData= new Patient_Messages(senderID,myTime,usersMessage);
         userMessages.child(messageKey).setValue(userMessagesData).addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -85,8 +128,54 @@ public class Chattingpage extends AppCompatActivity {
                 if(task.isSuccessful())
                 {
                     Toast.makeText(Chattingpage.this,"MESSAGE SAVED",Toast.LENGTH_SHORT).show();
+                   // Chattingpage.this.notify();
+                  readPatientMessages();
+                    layoutManager.setStackFromEnd(true);
+                    myRecycleViewer.scrollToPosition(myAdapter.getItemCount()-1);
+                }
+            }
+        });
+    }
+
+    private void readPatientMessages()
+    {
+        //create a query to get the messages from database
+ messages_from_user.clear();
+        Query query= userMessages.orderByChild("timestamp");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot childSnapshot:snapshot.getChildren())
+                {
+                    //check here no data is being read from db seems it is reading twice
+                    Patient_Messages messages= childSnapshot.getValue(Patient_Messages.class);
+                    assert messages != null;
+                    if(Objects.equals(messages.getSender_id(), "patient"))
+                    {
+
+                        messages_from_user.add(messages.getMessage());
+
+                    }
+                    else
+                    {
+
+                        messages_from_user.add("Doc : "+messages.getMessage());
+                        counter++;
+                    }
+
 
                 }
+
+                 myAdapter.setData(messages_from_user);
+                layoutManager.setStackFromEnd(true);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Chattingpage.this,"ERROR RETRIEVING MESSAGES, CHECK NETWORK!",Toast.LENGTH_SHORT).show();
+
             }
         });
     }
--- app/src/main/java/com/example/my_hospital_appointments/chattingAdapter.java
+++ app/src/main/java/com/example/my_hospital_appointments/chattingAdapter.java
@@ -15,8 +15,21 @@ import java.util.ArrayList;
 public class chattingAdapter extends RecyclerView.Adapter<chattingAdapter.myViewHolder> {
 
 private ArrayList<String> message=new ArrayList<>();
+private ArrayList<String> newMessage=new ArrayList<>();
+private ArrayList<String> userMessage=new ArrayList<>();
+private ArrayList <Patient_Messages> myMessageList=new ArrayList<>();
+private String myText;
+private static int myCounter=0;
+
     private static final int SENT_MESSAGE_TYPE = 0;
     private static final int RECEIVED_MESSAGE_TYPE = 1;
+
+    public void setData(ArrayList<String> myList) {
+        this.message=myList;
+
+      notifyDataSetChanged();
+
+    }
 
     public static class myViewHolder extends RecyclerView.ViewHolder {
     //public EditText myEditText;
@@ -27,22 +40,22 @@ private ArrayList<String> message=new ArrayList<>();
         //a constructor
         public myViewHolder(@NonNull View itemView, int viewType) {
             super(itemView);
-           if(viewType==SENT_MESSAGE_TYPE)
-           {
-               sentMessageTextView = (TextView) itemView.findViewById(R.id.sentTextViewMessage);
-           }
-           else
-           {
-               receivedMessageTextView =(TextView)  itemView.findViewById(R.id.receivedTextViewMessage);
-           }
+            if(viewType==SENT_MESSAGE_TYPE) {
+                sentMessageTextView = (TextView) itemView.findViewById(R.id.sentTextViewMessage);
+
+            }else {
+                receivedMessageTextView = (TextView) itemView.findViewById(R.id.receivedTextViewMessage);
+            }
+
 
 
         }
     }
 
-    public chattingAdapter(ArrayList<String> myMessage)
+    public chattingAdapter(ArrayList<String> myMessages)
     {
-         message=myMessage;
+       this.message =myMessages;
+
     }
 
     @NonNull
@@ -50,6 +63,8 @@ private ArrayList<String> message=new ArrayList<>();
     public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View myView;
+
+
         if(viewType==SENT_MESSAGE_TYPE )
         {
             myView= LayoutInflater.from(parent.getContext()).inflate(R.layout.recycle_viewer_item_sent_message,parent, false );
@@ -61,6 +76,8 @@ private ArrayList<String> message=new ArrayList<>();
 
         }
 
+
+
                 myViewHolder mv = new myViewHolder(myView,viewType);
 
         return mv;
@@ -70,17 +87,15 @@ private ArrayList<String> message=new ArrayList<>();
     @Override
     public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
 
-        if(getItemViewType(position)==SENT_MESSAGE_TYPE )
-        {
+        if (getItemViewType(position) == SENT_MESSAGE_TYPE) {
             holder.sentMessageTextView.setText(message.get(position));
-        }
-        else
-        {
+        } else {
             holder.receivedMessageTextView.setText(message.get(position));
         }
 
 
     }
+
 
     @Override
     public int getItemCount() {
@@ -90,15 +105,21 @@ private ArrayList<String> message=new ArrayList<>();
     @Override
     public int getItemViewType(int position) {   //we use the getItemViewType method to get the view to use in the recycle Viewer
 
-        if(message.get(position).contains("Sent_")) //IF in the arraylist the text has the words "Sent_" we know its from
+
+
+
+        if(message.get(position).contains("Doc : ")) //IF in the arraylist the text has the words "Sent_" we know its from
                                                     //the sender thus we return the sent_Message_type
         {                                     //the message will determine type of view to be used
-            return SENT_MESSAGE_TYPE;
+
+            return RECEIVED_MESSAGE_TYPE;
         }
         else
         {
-            return RECEIVED_MESSAGE_TYPE;
+            return SENT_MESSAGE_TYPE;
         }
+
+
 
 
     }
