a6658a844b932d723c15d4dca5f4fff6c0edf016
DALTON LEYIAN <daltonleyian@gmail.com>
2023-02-13 17:25:08 +0300

    Added a new UI for Doctor's main page to replace old one

---
+++ app/src/main/java/com/example/my_hospital_appointments/Doctor_Main_Page_NEW.java
@@ -0,0 +1,14 @@
+package com.example.my_hospital_appointments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Doctor_Main_Page_NEW extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_main_page_new);
+    }
+}
