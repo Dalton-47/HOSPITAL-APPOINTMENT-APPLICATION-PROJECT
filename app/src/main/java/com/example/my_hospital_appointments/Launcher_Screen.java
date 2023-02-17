5a0ac293e9de3197bacd76d315efa68e00f125d7
DALTON LEYIAN <daltonleyian@gmail.com>
2023-02-17 20:58:44 +0300

    UPDATE - Added new launcher screen

---
+++ app/src/main/java/com/example/my_hospital_appointments/Launcher_Screen.java
@@ -0,0 +1,14 @@
+package com.example.my_hospital_appointments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Launcher_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher_screen);
+    }
+}
