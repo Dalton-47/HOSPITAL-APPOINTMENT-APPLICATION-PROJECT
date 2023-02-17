782b9b3e4e316c9e70ace22689eb35d7771300e0
DALTON LEYIAN <daltonleyian@gmail.com>
2023-02-17 22:08:16 +0300

    UPDATE - Added new launcher screen

--- app/src/main/java/com/example/my_hospital_appointments/Launcher_Screen.java
+++ app/src/main/java/com/example/my_hospital_appointments/Launcher_Screen.java
@@ -3,12 +3,35 @@ package com.example.my_hospital_appointments;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.TextAppearanceSpan;
+import android.widget.TextView;
 
 public class Launcher_Screen extends AppCompatActivity {
-
+ TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher_screen);
+
+         textView = findViewById(R.id.textViewLauncher);
+        String text = "Appointments Scheduler";
+        SpannableString spannableString = new SpannableString(text);
+        int duration = 200; // Delay between each letter in milliseconds
+
+        for (int i = 0; i < text.length(); i++) {
+            TextAppearanceSpan appearanceSpan = new TextAppearanceSpan(this, R.style.TextAppearance);
+            spannableString.setSpan(appearanceSpan, i, i + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            final int finalI = i;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText(spannableString.subSequence(0, finalI + 1));
+                }
+            }, duration * i);
+        }
+
     }
 }
